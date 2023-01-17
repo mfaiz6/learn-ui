071ac014d219570e8c6f94d1069e1d9ee880f152
Mohammad Faiz Ahanger <Mohammadfaiz_2019bcse002@nitsri.net>
2023-01-17 17:42:41 +0530

    initial commit && add navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import './navbar.css'
+
+const Navbar = () => {
+
+    const [toggle, setToggle] = useState(false)
+
+ 
+
+    return (
+        <>
+            <div className="navContainer">
+
+
+                <div className="navLogo">
+                    <img src="https://snowkashmirholidays.com/images/logo.png" alt="logo" className='logo' />
+                </div>
+
+                <i className="fa-solid fa-bars toggleButton" onClick={() => setToggle(!toggle)}></i>
+
+                <div className={toggle ? "navItemsShow" : "navItems"}>
+
+                    <div className="navItem">Home</div>
+                    <div className="navItem">Packages</div>
+                    <div className="navItem">Blog</div>
+                    <div className="navItem">About Us</div>
+                    <div className="navItem">Contact Us</div>
+                    <div className="navItem">Sign in/up</div>
+
+
+                </div>
+
+            </div>
+        </>
+    )
+}
+
+export default Navbar
