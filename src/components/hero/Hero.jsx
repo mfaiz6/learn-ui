0d3e7dc6fd8cc93d8e4fb2b932d499543d5c83df
Mohammad Faiz Ahanger <Mohammadfaiz_2019bcse002@nitsri.net>
2023-01-18 20:25:27 +0530

    add hero component

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,23 @@
+import './hero.css'
+
+const Hero = () => {
+  return (
+    <>
+
+      <div className='heroContainer'>
+
+        <div className="heroText">
+
+          <h1 className="heroHeading">Explore Heaven On Earth!</h1>
+          
+          <button className="heroButton">See Packages</button>
+
+        </div>
+
+      </div>
+
+    </>
+  )
+}
+
+export default Hero
