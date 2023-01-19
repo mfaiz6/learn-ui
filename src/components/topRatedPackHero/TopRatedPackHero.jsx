880292a84cd89dbf8ae85a762fc5752309e16fd2
Mohammad Faiz Ahanger <Mohammadfaiz_2019bcse002@nitsri.net>
2023-01-19 14:58:16 +0530

    complete TopRatedPackHero Component

--- src/components/topRatedPackHero/TopRatedPackHero.jsx
+++ src/components/topRatedPackHero/TopRatedPackHero.jsx
@@ -1,3 +1,6 @@
+import { faStarHalf } from '@fortawesome/free-regular-svg-icons'
+import { faLocationDot, faStar, faStarHalfAlt } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './topRatedPackHero.css'
 
 const TopRatedPackHero = () => {
@@ -14,11 +17,24 @@ const TopRatedPackHero = () => {
                         <img src="https://res.cloudinary.com/thrillophilia/image/upload/c_fill,f_auto,fl_progressive.strip_profile,g_auto,h_600,q_auto,w_auto/v1/filestore/0kclx0s22i4skhf8xd92d2lqglaj_LADAKH%20%282%29.png" alt="" />
                     </div>
 
+                    <div className="topRatedPackCardDuration">
+                        <p>
+                            <FontAwesomeIcon icon={faLocationDot} />
+                            <span> Leh | 6D/5N</span>
+                        </p>
+                    </div>
+
                     <div className="topRatedPackCardText">
                         <h3 className='topRatedPackCardHead'>Thrilling Tour</h3>
-                        <p className="topRatedPackCardRating">5.0</p>
+                        <p className="topRatedPackCardRating"> 
+                        <FontAwesomeIcon icon={faStar} className="star" />
+                        <FontAwesomeIcon icon={faStar} className="star" />
+                        <FontAwesomeIcon icon={faStar} className="star" />
+                        <FontAwesomeIcon icon={faStar} className="star" />
+                        <FontAwesomeIcon icon={faStar} className="star" />
+                        </p>
                         <h4 className="topRatedPackCardOPrice">₹21999</h4>
-                        <h4 className="topRatedPackCardOPrice">₹17999</h4>
+                        <h4 className="topRatedPackCardNPrice">₹17999</h4>
                     </div>
 
                 </div>
@@ -29,11 +45,23 @@ const TopRatedPackHero = () => {
                         <img src="https://res.cloudinary.com/thrillophilia/image/upload/c_fill,f_auto,fl_progressive.strip_profile,g_auto,h_600,q_auto,w_auto/v1/filestore/jv997441dvnrzs4p4y5dkc3dntrv_1615985269_shutterstock_1455828107_%281%29.jpg.webp" alt="" />
                     </div>
 
+                    <div className="topRatedPackCardDuration">
+                        <p>
+                            <FontAwesomeIcon icon={faLocationDot} />
+                            <span> Leh | 6D/5N</span>
+                        </p>
+                    </div>
+
                     <div className="topRatedPackCardText">
                         <h3 className='topRatedPackCardHead'>Thrilling Tour</h3>
-                        <p className="topRatedPackCardRating">5.0</p>
+                        <p className="topRatedPackCardRating"> 
+                        <FontAwesomeIcon icon={faStar} className="star" />
+                        <FontAwesomeIcon icon={faStar} className="star" />
+                        <FontAwesomeIcon icon={faStar} className="star" />
+                        <FontAwesomeIcon icon={faStarHalfAlt} className="star" />
+                        </p>
                         <h4 className="topRatedPackCardOPrice">₹21999</h4>
-                        <h4 className="topRatedPackCardOPrice">₹17999</h4>
+                        <h4 className="topRatedPackCardNPrice">₹17999</h4>
                     </div>
 
                 </div>
@@ -44,11 +72,22 @@ const TopRatedPackHero = () => {
                         <img src="https://res.cloudinary.com/thrillophilia/image/upload/c_fill,f_auto,fl_progressive.strip_profile,g_auto,h_600,q_auto,w_auto/v1/filestore/w9poxafpk1yo337nsswvtf9gv3hr_2%20%286%29.jpg" alt="" />
                     </div>
 
+                    <div className="topRatedPackCardDuration">
+                        <p>
+                            <FontAwesomeIcon icon={faLocationDot} />
+                            <span> Leh | 6D/5N</span>
+                        </p>
+                    </div>
+
                     <div className="topRatedPackCardText">
                         <h3 className='topRatedPackCardHead'>Thrilling Tour</h3>
-                        <p className="topRatedPackCardRating">5.0</p>
+                        <p className="topRatedPackCardRating"> 
+                        <FontAwesomeIcon icon={faStar} className="star" />
+                        <FontAwesomeIcon icon={faStar} className="star" />
+                        <FontAwesomeIcon icon={faStar} className="star" />
+                        </p>
                         <h4 className="topRatedPackCardOPrice">₹21999</h4>
-                        <h4 className="topRatedPackCardOPrice">₹17999</h4>
+                        <h4 className="topRatedPackCardNPrice">₹17999</h4>
                     </div>
 
                 </div>
@@ -59,11 +98,22 @@ const TopRatedPackHero = () => {
                         <img src="https://res.cloudinary.com/thrillophilia/image/upload/c_fill,f_auto,fl_progressive.strip_profile,g_auto,h_600,q_auto,w_auto/v1/filestore/749sclgvs7qjknke5sbz9mbbdabh_shutterstock_1621541545.jpg" alt="" />
                     </div>
 
+                    <div className="topRatedPackCardDuration">
+                        <p>
+                            <FontAwesomeIcon icon={faLocationDot} />
+                            <span> Leh | 6D/5N</span>
+                        </p>
+                    </div>
+
                     <div className="topRatedPackCardText">
                         <h3 className='topRatedPackCardHead'>Thrilling Tour</h3>
-                        <p className="topRatedPackCardRating">5.0</p>
+                        <p className="topRatedPackCardRating"> 
+                        <FontAwesomeIcon icon={faStar} className="star" />
+                        <FontAwesomeIcon icon={faStar} className="star" />
+                        <FontAwesomeIcon icon={faStar} className="star" />
+                        </p>
                         <h4 className="topRatedPackCardOPrice">₹21999</h4>
-                        <h4 className="topRatedPackCardOPrice">₹17999</h4>
+                        <h4 className="topRatedPackCardNPrice">₹17999</h4>
                     </div>
 
                 </div>
