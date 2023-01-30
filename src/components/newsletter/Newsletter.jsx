f50b04704753662bfeb777d7d8ac8155efb1f302
mfaiz6 <Mohammadfaiz_2019bcse002@nitsri.net>
2023-01-30 18:25:12 +0530

    minor fix in newsletter component

--- src/components/newsletter/Newsletter.jsx
+++ src/components/newsletter/Newsletter.jsx
@@ -1,7 +1,10 @@
 import './newsletter.css'
 
 const Newsletter = () => {
+
+
     return (
+
         <>
 
             <div className="nLetterContainer">
@@ -10,10 +13,12 @@ const Newsletter = () => {
 
                     <h2 className="nLetterHeading">Subscribe to Newsletter</h2>
 
-                    <input type="email" className="nLetterInput" placeholder='email' />
+                    <form>
 
-                    <button className="nLetterButton">Subscribe</button>
+                        <input type="email" className="nLetterInput" placeholder='email' pattern="^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$" required />
 
+                        <button type='submit' className="nLetterButton">Subscribe</button>
+                    </form>
                 </div>
 
             </div>
