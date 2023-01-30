aff80ca552286bafafe97aaf693f5eff943914d7
mfaiz6 <Mohammadfaiz_2019bcse002@nitsri.net>
2023-01-30 19:18:45 +0530

    add newsletter functionality component

--- src/components/newsletter/Newsletter.jsx
+++ src/components/newsletter/Newsletter.jsx
@@ -1,6 +1,30 @@
+import axios from 'axios'
+import { useState } from 'react'
 import './newsletter.css'
 
 const Newsletter = () => {
+
+    const [email, setEmail] = useState("")
+
+    const handleChange = (e) => {
+        setEmail(e.target.value)
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            await axios.post("/newsLetter/", {
+                "email": email
+            })
+            document.getElementById('newsLetterSuccess').innerHTML = "Subscribed!"
+
+
+        } catch (error) {
+            document.getElementById('newsLetterSuccess').innerHTML = "Already there!"
+
+        }
+
+    }
 
 
     return (
@@ -13,12 +37,14 @@ const Newsletter = () => {
 
                     <h2 className="nLetterHeading">Subscribe to Newsletter</h2>
 
-                    <form>
+                    <form onSubmit={handleSubmit}>
 
-                        <input type="email" className="nLetterInput" placeholder='email' pattern="^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$" required />
+                        <input type="email" className="nLetterInput" placeholder='email' pattern="^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$" required onChange={handleChange} />
 
-                        <button type='submit' className="nLetterButton">Subscribe</button>
+                        <button type='submit' className="nLetterButton" id='newsLetterSuccess' >Subscribe</button>
+
                     </form>
+
                 </div>
 
             </div>
